c8f5acd3265febe4cdfe66d9741efb89f29ef46b
ricardo-a-alves-alb <ricardo-a-alves@alticelabs.com>
2024-11-19 19:30:37 +0000

    remove setTimeout and use state to open info page

--- app/open-info-handler.tsx
+++ app/open-info-handler.tsx
@@ -1,17 +1,18 @@
 'use client'
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function OpenInfoHandler() {
   const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
+  const [infoPath, setInfoPath] = useState<string | null>(null)
   
   useEffect(() => {
     // current url has info query string parameter?
-    const infoPath = searchParams.get('info')
-    if (!infoPath) {
+    const qsInfo = searchParams.get('info')
+    if (!qsInfo) {
       return
     }
 
@@ -22,9 +23,18 @@ export function OpenInfoHandler() {
     // replace the current url to remove the search parameter
     router.replace(`${pathname}?${newSearchParams}`)
 
-    // allow previous replace to take place before pushing new route
-    setTimeout(() => router.push(infoPath), 100)
+    // save info url in state 
+    setInfoPath(qsInfo)
   }, [pathname, searchParams])
+
+  useEffect(() => {
+    if (!infoPath) {
+      return
+    }
+
+    // navigate to info
+    router.push(infoPath)
+  }, [infoPath])
   
   return null
 }
